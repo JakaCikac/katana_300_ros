9f55af88278130a0a4a420d6a320ca53b5ab1809
JakaCikac <nanorax@gmail.com>
2014-04-03 13:05:25 +0200

    Initial commit.

--- katana_tutorials/include/katana_tutorials/follow_joint_trajectory_client.h
+++ katana_tutorials/include/katana_tutorials/follow_joint_trajectory_client.h
@@ -12,12 +12,21 @@
 #include <control_msgs/FollowJointTrajectoryAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <sensor_msgs/JointState.h>
+//#include <arm_navigation_msgs/FilterJointTrajectory.h>
 #include <trajectory_msgs/JointTrajectory.h>
+#include <control_msgs/GripperCommandAction.h>
+
+#include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
+
+#define GRASP 1
+#define RELEASE 2
 
 namespace katana_tutorials
 {
 
 typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> TrajClient;
+typedef control_msgs::GripperCommandGoal GCG;
 
 class FollowJointTrajectoryClient
 {
@@ -26,19 +35,31 @@ public:
   virtual ~FollowJointTrajectoryClient();
 
   void startTrajectory(control_msgs::FollowJointTrajectoryGoal goal);
+  
   control_msgs::FollowJointTrajectoryGoal makeArmUpTrajectory();
+  control_msgs::FollowJointTrajectoryGoal makeArmUpTrajectoryBack();
   actionlib::SimpleClientGoalState getState();
+  bool send_gripper_action(int32_t goal_type);
 
 private:
   ros::NodeHandle nh_;
+  ros::NodeHandle p;
+  ros::Publisher end_pub;
+  ros::Publisher end_part2_pub;
   TrajClient traj_client_;
+  actionlib::SimpleActionClient<control_msgs::GripperCommandAction> gripper_;
   ros::Subscriber joint_state_sub_;
+  ros::Subscriber start_sub;
+  ros::Subscriber start_part2_sub;
   std::vector<std::string> joint_names_;
   bool got_joint_state_;
   std::vector<double> current_joint_state_;
   ros::AsyncSpinner spinner_;
 
+  void startTrajectoryCallback(const std_msgs::Bool::ConstPtr& msg);
+  void startTrajectoryBackCallback(const std_msgs::Bool::ConstPtr& msg);
   void jointStateCB(const sensor_msgs::JointState::ConstPtr &msg);
+  //trajectory_msgs::JointTrajectory filterJointTrajectory(const trajectory_msgs::JointTrajectory &input);
 };
 
 } /* namespace katana_tutorials */
--- katana_tutorials/src/follow_joint_trajectory_client.cpp
+++ katana_tutorials/src/follow_joint_trajectory_client.cpp
@@ -3,15 +3,18 @@
  *
  *  Created on: 06.11.2011
  *      Author: martin
+ *  Modified on: 28.02.2014
+ * 	Author: JakaCikac
  */
 
 #include <katana_tutorials/follow_joint_trajectory_client.h>
+#include "ros/ros.h"
 
 namespace katana_tutorials
 {
 
 FollowJointTrajectoryClient::FollowJointTrajectoryClient() :
-    traj_client_("/katana_arm_controller/follow_joint_trajectory", true), got_joint_state_(false), spinner_(1)
+    traj_client_("/katana_arm_controller/follow_joint_trajectory", true), gripper_("gripper_grasp_posture_controller",true), got_joint_state_(false), spinner_(1)
 {
   joint_names_.push_back("katana_motor1_pan_joint");
   joint_names_.push_back("katana_motor2_lift_joint");
@@ -20,17 +23,77 @@ FollowJointTrajectoryClient::FollowJointTrajectoryClient() :
   joint_names_.push_back("katana_motor5_wrist_roll_joint");
 
   joint_state_sub_ = nh_.subscribe("/joint_states", 1, &FollowJointTrajectoryClient::jointStateCB, this);
+  start_sub = nh_.subscribe("katana/start_trajectory", 1, &FollowJointTrajectoryClient::startTrajectoryCallback, this);
+  start_part2_sub = nh_.subscribe("katana/start_trajectory2", 1, &FollowJointTrajectoryClient::startTrajectoryBackCallback, this);
+  end_part2_pub = p.advertise<std_msgs::Bool>("katana/retracted2", 1);
+  end_pub = p.advertise<std_msgs::Bool>("katana/retracted", 1);
+  
   spinner_.start();
-
+  
   // wait for action server to come up
   while (!traj_client_.waitForServer(ros::Duration(5.0)))
   {
     ROS_INFO("Waiting for the follow_joint_trajectory server");
   }
+    ROS_INFO("Received trajectory client server.");  
+  
+  // wait for action server to come up
+  while (!gripper_.waitForServer(ros::Duration(5.0)))
+  {
+    ROS_INFO("Waiting for the gripper_grasp_posture_controller server");
+  }
+    ROS_INFO("Received gripper_grasp server.");
 }
 
 FollowJointTrajectoryClient::~FollowJointTrajectoryClient()
 {
+}
+
+//static katana_tutorials::FollowJointTrajectoryClient arm_static;
+// Start trajectory when requested
+void FollowJointTrajectoryClient::startTrajectoryCallback(const std_msgs::Bool::ConstPtr& msg)
+{
+      
+      // Publisher end_pub publishes a signal to navigation when the katana ends trajectory
+      std_msgs::Bool pub_msg;
+      pub_msg.data = 1;
+      // Init the TrajectoryClient
+      //arm.send_gripper_action(GRASP);
+        // Start the trajectory
+      startTrajectory(makeArmUpTrajectory());
+      while (!getState().isDone()) //&& ros::ok())
+      {
+	  // Suspend execution of the calling thread for (atleast) usec microseconds.
+	  // This means, the trajectory will be completed even if the sigterm is sent to this node.
+	  usleep(50000);
+      }
+      
+      // publish a signal, that notifies the end of trajectory
+     end_pub.publish(pub_msg);
+}
+
+// Start trajectory when requested
+void FollowJointTrajectoryClient::startTrajectoryBackCallback(const std_msgs::Bool::ConstPtr& msg)
+{
+
+      // Publisher end_pub publishes a signal to navigation when the katana ends trajectory
+      std_msgs::Bool pub_msg;
+      pub_msg.data = 1;
+      ROS_INFO("Calling back trajectory.");
+      // Init the TrajectoryClient
+      
+      //arm.send_gripper_action(GRASP);
+      // Start the trajectory
+      startTrajectory(makeArmUpTrajectoryBack());
+      while (!getState().isDone())
+      {
+	  // Suspend execution of the calling thread for (atleast) usec microseconds.
+	  // This means, the trajectory will be completed even if the sigterm is sent to this node.
+	  usleep(50000);
+      }
+      
+      // publish a signal, that notifies the end of trajectory
+      end_part2_pub.publish(pub_msg);
 }
 
 void FollowJointTrajectoryClient::jointStateCB(const sensor_msgs::JointState::ConstPtr &msg)
@@ -70,24 +133,26 @@ void FollowJointTrajectoryClient::startTrajectory(control_msgs::FollowJointTraje
 
 control_msgs::FollowJointTrajectoryGoal FollowJointTrajectoryClient::makeArmUpTrajectory()
 {
-  const size_t NUM_TRAJ_POINTS = 4;
+  const size_t NUM_TRAJ_POINTS = 3;
   const size_t NUM_JOINTS = 5;
 
   // positions after calibration
   std::vector<double> calibration_positions(NUM_JOINTS);
-  calibration_positions[0] = -2.96;
-  calibration_positions[1] = 2.14;
-  calibration_positions[2] = -2.16;
-  calibration_positions[3] = -1.97;
-  calibration_positions[4] = -2.93;
+  calibration_positions[0] = -2.90;
+  calibration_positions[1] = 2.10;
+  calibration_positions[2] = -2.15;
+  calibration_positions[3] = -1.91;
+  calibration_positions[4] = -2.87;
 
-  // arm pointing straight up
-  std::vector<double> straight_up_positions(NUM_JOINTS);
-  straight_up_positions[0] = 0.0;
-  straight_up_positions[1] = 1.57;
-  straight_up_positions[2] = 0.0;
-  straight_up_positions[3] = 0.0;
-  straight_up_positions[4] = 0.0;
+  // arm pointing forward
+  std::vector<double> backwards_positions(NUM_JOINTS);
+  backwards_positions[0] = 0.047;
+  backwards_positions[1] = 1.0;
+  backwards_positions[2] = -0.23;
+  backwards_positions[3] = -0.92;
+  backwards_positions[4] = -1.57;
+
+//0.047342055960345686, 2.159243669302496, -0.2366207493507151, -0.9283515374436089, -1.5740006422184796, -0.054775763491041185, -0.054775763491041185
 
   trajectory_msgs::JointTrajectory trajectory;
 
@@ -106,38 +171,80 @@ control_msgs::FollowJointTrajectoryGoal FollowJointTrajectoryClient::makeArmUpTr
 
   // trajectory point:
   int ind = 0;
-  trajectory.points[ind].time_from_start = ros::Duration(5 * ind);
+  trajectory.points[ind].time_from_start = ros::Duration(ind);
+  trajectory.points[ind].positions = current_joint_state_;
+ 
+  // trajectory point:
+  ind++;
+  trajectory.points[ind].time_from_start = ros::Duration(ind+1);
   trajectory.points[ind].positions = current_joint_state_;
 
   // trajectory point:
   ind++;
-  trajectory.points[ind].time_from_start = ros::Duration(5 * ind);
-  trajectory.points[ind].positions = calibration_positions;
+  trajectory.points[ind].time_from_start = ros::Duration(ind+2);
+  trajectory.points[ind].positions.resize(NUM_JOINTS);
+  trajectory.points[ind].positions = backwards_positions;
+  
+  
+  control_msgs::FollowJointTrajectoryGoal goal;
+  goal.trajectory = trajectory;//filterJointTrajectory(trajectory);
+  return goal;
+  
+}
+
+control_msgs::FollowJointTrajectoryGoal FollowJointTrajectoryClient::makeArmUpTrajectoryBack()
+{
+  const size_t NUM_TRAJ_POINTS = 2;
+  const size_t NUM_JOINTS = 5;
+
+  // positions after calibration
+  std::vector<double> calibration_positions(NUM_JOINTS);
+  calibration_positions[0] = 0.5;
+  calibration_positions[1] = 2.10;
+  calibration_positions[2] = -2.15;
+  calibration_positions[3] = -1.91;
+  calibration_positions[4] = -2.87;
+
+  // arm pointing forward
+  std::vector<double> backwards_positions(NUM_JOINTS);
+  backwards_positions[0] = 0.047;
+  backwards_positions[1] = 2.15;
+  backwards_positions[2] = -0.23;
+  backwards_positions[3] = -0.92;
+  backwards_positions[4] = -1.57;
+
+//0.047342055960345686, 2.159243669302496, -0.2366207493507151, -0.9283515374436089, -1.5740006422184796, -0.054775763491041185, -0.054775763491041185
+
+  trajectory_msgs::JointTrajectory trajectory;
+
+  for (ros::Rate r = ros::Rate(10); !got_joint_state_; r.sleep())
+  {
+    ROS_DEBUG("waiting for joint state...");
+
+    if (!ros::ok())
+      exit(-1);
+  }
+
+  // First, the joint names, which apply to all waypoints
+  trajectory.joint_names = joint_names_;
+
+  trajectory.points.resize(NUM_TRAJ_POINTS);
+
+  // trajectory point:
+  int ind = 0;
+  trajectory.points[ind].time_from_start = ros::Duration(ind);
+  trajectory.points[ind].positions = current_joint_state_;
 
   // trajectory point:
   ind++;
-  trajectory.points[ind].time_from_start = ros::Duration(5 * ind);
-  trajectory.points[ind].positions = straight_up_positions;
-
-  // trajectory point:
-  ind++;
-  trajectory.points[ind].time_from_start = ros::Duration(5 * ind);
+  trajectory.points[ind].time_from_start = ros::Duration(ind+7);
   trajectory.points[ind].positions.resize(NUM_JOINTS);
   trajectory.points[ind].positions = calibration_positions;
 
-  //  // all Velocities 0
-  //  for (size_t i = 0; i < NUM_TRAJ_POINTS; ++i)
-  //  {
-  //    trajectory.points[i].velocities.resize(NUM_JOINTS);
-  //    for (size_t j = 0; j < NUM_JOINTS; ++j)
-  //    {
-  //      trajectory.points[i].velocities[j] = 0.0;
-  //    }
-  //  }
-
   control_msgs::FollowJointTrajectoryGoal goal;
-  goal.trajectory = trajectory;
+  goal.trajectory = trajectory;//filterJointTrajectory(trajectory);
   return goal;
+  
 }
 
 //! Returns the current state of the action
@@ -146,19 +253,89 @@ actionlib::SimpleClientGoalState FollowJointTrajectoryClient::getState()
   return traj_client_.getState();
 }
 
+/* trajectory_msgs::JointTrajectory FollowJointTrajectoryClient::filterJointTrajectory(
+    const trajectory_msgs::JointTrajectory &input)
+{
+  ros::service::waitForService("trajectory_filter/filter_trajectory");
+  arm_navigation_msgs::FilterJointTrajectory::Request req;
+  arm_navigation_msgs::FilterJointTrajectory::Response res;
+  ros::ServiceClient filter_trajectory_client_ = nh_.serviceClient<arm_navigation_msgs::FilterJointTrajectory>(
+      "trajectory_filter/filter_trajectory");
+
+  req.trajectory = input;
+  req.allowed_time = ros::Duration(1.0);
+
+  if (filter_trajectory_client_.call(req, res))
+  {
+    if (res.error_code.val == res.error_code.SUCCESS)
+      ROS_INFO("Requested trajectory was filtered");
+    else
+      ROS_WARN("Requested trajectory was not filtered. Error code: %d", res.error_code.val);
+  }
+  else
+  {
+    ROS_ERROR("Service call to filter trajectory failed %s", filter_trajectory_client_.getService().c_str());
+  }
+
+  return res.trajectory;
+} */
+
+ bool FollowJointTrajectoryClient::send_gripper_action(int goal_type)
+{
+  GCG goal;
+
+  switch (goal_type)
+  {
+    case GRASP:
+      goal.command.position = -0.44; 
+      // leave velocity and effort empty
+      break;
+
+    case RELEASE:
+      goal.command.position = 0.3; 
+      // leave velocity and effort empty
+      break;
+
+    default:
+      ROS_ERROR("unknown goal code (%d)", goal_type);
+      return false;
+  }
+
+  bool finished_within_time = false;
+  gripper_.sendGoal(goal);
+  finished_within_time = gripper_.waitForResult(ros::Duration(10.0));
+  if (!finished_within_time)
+  {
+    gripper_.cancelGoal();
+    ROS_WARN("Timed out achieving goal!");
+    return false;
+  }
+  else
+  {
+    actionlib::SimpleClientGoalState state = gripper_.getState();
+    bool success = (state == actionlib::SimpleClientGoalState::SUCCEEDED);
+    if (success)
+      ROS_INFO("Action finished: %s",state.toString().c_str());
+    else
+      ROS_WARN("Action failed: %s",state.toString().c_str());
+
+    return success;
+  }
+ }
+
 } /* namespace katana_tutorials */
 
 int main(int argc, char** argv)
 {
   // Init the ROS node
   ros::init(argc, argv, "follow_joint_trajectory_client");
+  // Subscriber listens for signal from navigation to start trajectory
+  ros::NodeHandle n;
 
-  katana_tutorials::FollowJointTrajectoryClient arm;
-  // Start the trajectory
-  arm.startTrajectory(arm.makeArmUpTrajectory());
-  // Wait for trajectory completion
-  while (!arm.getState().isDone() && ros::ok())
-  {
-    usleep(50000);
-  }
+  // Call constructor and create master object
+  katana_tutorials::FollowJointTrajectoryClient armGripper;
+  armGripper.send_gripper_action(GRASP);
+  // start ros while loop
+  ros::spin();
+  return 0;
 }
